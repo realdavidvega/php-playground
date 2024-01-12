1fe75ae55092782bd12dc3e60f9e253847550d9d
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-01-12 21:54:58 +0100

    refactor: remove images, add init scripts

---
+++ code/my-shop/scripts/init_db.sh
@@ -0,0 +1,4 @@
+docker exec -it my-shop-mysql-1 mysql -u root -psomeRootPassword -e "INSERT INTO myShop.products (id, name, price, image) VALUES (1, 'Golden Skull', 14.99, 'golden-skull');"
+docker exec -it my-shop-mysql-1 mysql -u root -psomeRootPassword -e "INSERT INTO myShop.products (id, name, price, image) VALUES (2, 'White Tiger', 14.99, 'white-tiger');"
+docker exec -it my-shop-mysql-1 mysql -u root -psomeRootPassword -e "INSERT INTO myShop.products (id, name, price, image) VALUES (3, 'Patriot', 14.99, 'patriot');"
+docker exec -it my-shop-mysql-1 mysql -u root -psomeRootPassword -e "INSERT INTO myShop.products (id, name, price, image) VALUES (4, 'Drama Queen', 14.99, 'drama-queen');"
